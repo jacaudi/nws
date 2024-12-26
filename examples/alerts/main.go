0150ee219a91142234d1cd89f111028c94cfeb3e
jacaudi <47005674+jacaudi@users.noreply.github.com>
2024-12-26 23:40:54 +0000

    chore(alerts): added more fun examples

--- examples/alerts/main.go
+++ examples/alerts/main.go
@@ -25,8 +25,24 @@ func main() {
 		fmt.Printf("Active Alert Details: %+v\n\n", activeAlerts)
 	}
 
-	firstAlertDesc := activeAlerts.Data[0].Description
-
 	// Print the Description of the First Alert Returned
-	fmt.Printf("***-----ALERT EXAMPLE-----***\n%s\n***END***\n", firstAlertDesc)
+	fmt.Printf("***-----ALERT EXAMPLE-----***\n%s\n***END***\n", activeAlerts.Data[0].Description)
+
+	// Pull the VTEC of the First 10 Alerts
+	count := 0
+	for _, alert := range activeAlerts.Data {
+		if count >= 10 {
+			break
+		}
+		if len(alert.Parameters.VTEC) == 0 {
+			continue
+		}
+		vtec := fmt.Sprintf("VTEC of Alert %02d: %s\n", count+1, alert.Parameters.VTEC)
+		fmt.Printf(vtec)
+		count++
+	}
+
+	// Calculate the total number of alerts
+	totalAlerts := len(activeAlerts.Data)
+	fmt.Printf("Total number of alerts: %d\n", totalAlerts)
 }
