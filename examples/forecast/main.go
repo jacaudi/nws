e85a7fb4960bbedcdd2bf81df11735758697d817
jacaudi <47005674+jacaudi@users.noreply.github.com>
2024-12-26 21:33:03 +0000

    chore(examples): add forecast example

---
+++ examples/forecast/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/jacaudi/nwsgo"
+)
+
+var (
+	debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+)
+
+func main() {
+	// Define the Lat & Lon
+	latlon := "47.445259,-122.294533"
+
+	pointData, err := nwsgo.GetPoints(latlon)
+	if err != nil {
+		log.Fatalf("Failed to get data from GPS location: %v", err)
+	}
+
+	if debug {
+		log.Printf("Points Endpoint Response: %v\n\n", pointData)
+	}
+
+	wfo := pointData.GridID
+	gridpoint := fmt.Sprintf("%s,%s", strconv.FormatFloat(pointData.GridX, 'f', -1, 64), strconv.FormatFloat(pointData.GridY, 'f', -1, 64))
+
+	if debug {
+		log.Printf("wfo: %s", wfo)
+		log.Printf("gridpoint: %s", gridpoint)
+	}
+
+	// Get the radar station details for KATX
+	forecastResponse, err := nwsgo.GetForecast(wfo, gridpoint)
+	if err != nil {
+		log.Fatalf("Failed to get radar station details: %v", err)
+	}
+
+	if debug {
+		log.Printf("Forecast Endpoint Response: %v\n\n", forecastResponse)
+	}
+
+	todayForecast := fmt.Sprintf("%s -- %s\n", forecastResponse.Periods[0].Name, forecastResponse.Periods[0].ShortForecast)
+	allForecast := ""
+	for _, period := range forecastResponse.Periods {
+		if period.IsDaytime {
+			allForecast += fmt.Sprintf("%s -- High Temperature: %s\n", period.Name, strconv.Itoa(period.Temperature))
+		} else {
+			allForecast += fmt.Sprintf("%s -- Low Temperature: %s\n", period.Name, strconv.Itoa(period.Temperature))
+		}
+	}
+
+	// Print the Short Forecast
+	fmt.Printf("Forecast: %s\n", todayForecast)
+	fmt.Printf("All Forecasts:\n%s", allForecast)
+}
