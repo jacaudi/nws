e706488b748493a949816a22904af2e60ef7e659
jacaudi <47005674+jacaudi@users.noreply.github.com>
2024-12-31 19:54:04 +0000

    chore: update repo structure

--- tests/nws_test.go
+++ tests/nws_test.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/jacaudi/nwsgo/cmd/nwsgo"
 	"github.com/jacaudi/nwsgo/internal/endpoints/radar"
 )
 
@@ -14,7 +15,7 @@ func TestRadarStation(t *testing.T) {
 	expectedResponse := &radar.RadarStationResponse{
 		// Define the expected response here
 	}
-	response, err := RadarStation(stationID)
+	response, err := nwsgo.RadarStation(stationID)
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
@@ -25,7 +26,7 @@ func TestRadarStation(t *testing.T) {
 	// Test case 2: Error response
 	stationID = "XYZ789"
 	expectedError := fmt.Errorf("failed to unmarshal response: some error")
-	response, err = RadarStation(stationID)
+	response, err = nwsgo.RadarStation(stationID)
 	if err == nil {
 		t.Errorf("Expected error, but got nil")
 	}
