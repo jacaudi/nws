b6725fabd51918f22cf92d0c321a30aec370fa9d
jacaudi <47005674+jacaudi@users.noreply.github.com>
2024-12-31 20:13:41 +0000

    feat: break apart the client and config into separate files

---
+++ cmd/nwsgo/config.go
@@ -0,0 +1,35 @@
+package nwsgo
+
+// Config instance for the API calls executed by the NWS client.
+var config = getDefaultConfig(Config{})
+
+// Config describes important values for the NWS API and handles making HTTP requests.
+type Config struct {
+	BaseURL   string `json:"baseUrl"` // Do not include a trailing slash
+	UserAgent string `json:"userAgent"`
+	Accept    string `json:"accept"`
+	Units     string `json:"units"`
+}
+
+// GetDefaultConfig returns the default configuration for the weather.gov API,
+// optionally merging it with a custom configuration.
+func getDefaultConfig(custom Config) Config {
+	defaultConfig := Config{
+		BaseURL:   "https://api.weather.gov",
+		UserAgent: "nwsgo/0.0.2 (+https://github.com/jacaudi/nwsgo)",
+		Accept:    "application/ld+json",
+		Units:     "", // Defaults to US units if unspecified
+	}
+
+	if custom.UserAgent != "" {
+		defaultConfig.UserAgent = custom.UserAgent
+	}
+	if custom.Accept != "" {
+		defaultConfig.Accept = custom.Accept
+	}
+	if custom.Units != "" {
+		defaultConfig.Units = custom.Units
+	}
+
+	return defaultConfig
+}
--- cmd/nwsgo/http.go
+++ cmd/nwsgo/http.go
@@ -5,39 +5,11 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 )
 
-// Show the HTTP response
-var httpResponse = false
-
-// httpLog is a boolean variable that determines whether HTTP logging is enabled or disabled.
-var httpLog = false
-
-// Config instance for the API calls executed by the NWS client.
-var config = GetDefaultConfig()
-
-// Config describes important values for the NWS API and handles making HTTP requests.
-type Config struct {
-	BaseURL   string `json:"baseUrl"` // Do not include a trailing slash
-	UserAgent string `json:"userAgent"`
-	Accept    string `json:"accept"`
-	Units     string `json:"units"`
-}
-
-// GetDefaultConfig returns the default configuration for the weather.gov API.
-func GetDefaultConfig() Config {
-	return Config{
-		BaseURL:   "https://api.weather.gov",
-		UserAgent: "nwsgo/0.0.2 (+https://github.com/jacaudi/nwsgo)",
-		Accept:    "application/ld+json",
-		Units:     "", // Defaults to US units if unspecified
-	}
-}
-
 // httpRequest makes an HTTP request to the NWS API and returns the response body.
-func (c *Config) httpRequest(url string) ([]byte, error) {
+func (c *Config) httpRequest(url string, httpResponse bool) ([]byte, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create request: %w", err)
@@ -68,17 +40,6 @@ func (c *Config) httpRequest(url string) ([]byte, error) {
 
 	if httpResponse {
 		log.Printf("Response body: %s", string(body))
-		if httpLog {
-			// Write log output to a file
-			logFile, err := os.OpenFile("HTTP_Response.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				return nil, fmt.Errorf("failed to open log file: %w", err)
-			}
-			defer logFile.Close()
-
-			log.SetOutput(io.MultiWriter(os.Stdout, logFile))
-			log.Println(string(body))
-		}
 	}
 
 	return body, nil
